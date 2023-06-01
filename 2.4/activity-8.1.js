6241f20001567df3591c61eae43d72971266273e
Samuel Kynaston <124631801+SamKynaston@users.noreply.github.com>
2023-06-01 10:39:43 +0100

    Map Function for 8

---
+++ 2.4/activity-8.1.js
@@ -0,0 +1,5 @@
+let bobsFollowers = ["Marsh", "John", "Smith", "Edward"]
+let hannahsFollowers = ["Marsh", "Edward", "John", "Arben"]
+
+bobsFollowers.map(item => console.log(item))
+hannahsFollowers.map(item => console.log(item))
